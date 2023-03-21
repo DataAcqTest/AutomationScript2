42e47ce8de93ed28fb74640ec7f30f3f4b02b625
Pedro Debevere <pedro.debevere@skyline.be>
2023-03-21 11:53:12 +0100

    Add project files.

---
+++ AutmationTest_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutmationTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AutmationTest_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("BC2815AD-2D76-4046-AEE7-2640649123F8")]
+[assembly: AssemblyVersion("1.0.0.0")]
